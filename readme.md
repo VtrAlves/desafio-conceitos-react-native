40f3026f70a9ed2718482a32662f5dcbf3fe4471
Vitor Alves <vitoralves35@outlook.com>
2020-04-13 19:47:47 -0300

    finished: Challenge

---
+++ readme.md
@@ -0,0 +1,43 @@
+<h3 align="center">
+  Desafio GoStack: Conceitos de React Native
+</h3>
+<p align="center">
+  <img alt="License" src="https://img.shields.io/badge/License-MIT-%2304D361">
+  <img alt="GitHub language count" src="https://img.shields.io/github/languages/count/VtrAlves/desafio-conceitos-react-native?color=%2304D361">
+  <img alt="MadeBy" src="https://img.shields.io/badge/made%20by-Vitor%20Alves-%2304D361">
+</p>
+
+## ❕ Sobre o desafio
+
+Esse desafio foi solicitado pela Rocketseat com as instruções passadas <a href= "https://github.com/Rocketseat/gostack-template-conceitos-react-native"> nesse repositório </a>.
+
+## 💻 Alterações do desafio
+
+Foi baixado o <a href="https://github.com/Rocketseat/gostack-template-conceitos-react-native">template</a> enviado e assim criei as funcionalidades de listagem / adição de likes para os repositórios enviando requisições para a API gerada no <a href="https://github.com/VtrAlves/desafio-conceitos-nodejs">desafio de Node.JS</a>.
+
+## 📖 Bibliotecas Utilizadas
+
+- `Axios`: Utilizado para realizar requisições ao servidor de backend.
+- `Hooks`: Integrado ao react e Utilizado para criação de variáveis de estado e controle de funções
+- `Jest`: Utilizado para realização de testes automatizados na aplicação. (Instalado em ambiente de desenvolvimento)
+
+## 🚀 Iniciar aplicação
+
+Primeiramente é necessário baixar as dependências com o comando:
+
+> yarn
+
+Após a instalação, para iniciar o servidor utilize o comando:
+
+** Caso você esteja emulando no iOS, na pasta do seu projeto navegue até a pasta ios executando o comando _cd ios_ e depois execute _pod install_ para instalar todas as dependências para o iOS. **
+
+> yarn android
+> yarn iOS
+
+Caso deseja rodar os testes, utilize o seguinte comando:
+
+> yarn test
+
+---
+
+Obrigado!
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
 
 import {
   SafeAreaView,
@@ -7,101 +7,139 @@ import {
   Text,
   StatusBar,
   StyleSheet,
-  TouchableOpacity,
-} from "react-native";
+  TouchableOpacity
+} from 'react-native'
 
-export default function App() {
-  async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+import api from './services/api'
+
+export default function App () {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
+  async function handleLikeRepository (id) {
+    try {
+      const { data } = await api.post(`repositories/${id}/like`)
+
+      const newRepositories = repositories.map(repository =>
+        repository.id === id
+          ? {
+              id,
+              title: repository.title,
+              url: repository.url,
+              techs: repository.techs,
+              likes: data.likes
+            }
+          : repository
+      )
+
+      setRepositories(newRepositories)
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <StatusBar barStyle='light-content' backgroundColor='#7159c1' />
+
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
+        <FlatList
+          // style={styles.repositoryContainer}
+          data={repositories}
+          keyExtractor={repository => repository.id}
+          renderItem={({ item: repository }) => {
+            const { title, techs, likes, id } = repository
+            return (
+              <View style={styles.repositoryContainer}>
+                <Text style={styles.repository}>{title}</Text>
 
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
+                <View style={styles.techsContainer}>
+                  {techs.map(techName => (
+                    <Text style={styles.tech} key={techName}>
+                      {techName}
+                    </Text>
+                  ))}
+                </View>
 
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
+                <View style={styles.likesContainer}>
+                  <Text
+                    style={styles.likeText}
+                    // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
+                    testID={`repository-likes-${id}`}
+                  >
+                    {likes} curtidas
+                  </Text>
+                </View>
 
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={() => handleLikeRepository(id)}
+                  // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
+                  testID={`like-button-${id}`}
+                >
+                  <Text style={styles.buttonText}>Curtir</Text>
+                </TouchableOpacity>
+              </View>
+            )
+          }}
+        />
       </SafeAreaView>
     </>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#7159c1",
+    backgroundColor: '#7159c1'
   },
   repositoryContainer: {
     marginBottom: 15,
     marginHorizontal: 15,
-    backgroundColor: "#fff",
-    padding: 20,
+    backgroundColor: '#fff',
+    padding: 20
   },
   repository: {
     fontSize: 32,
-    fontWeight: "bold",
+    fontWeight: 'bold'
   },
   techsContainer: {
-    flexDirection: "row",
-    marginTop: 10,
+    flexDirection: 'row',
+    marginTop: 10
   },
   tech: {
     fontSize: 12,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginRight: 10,
-    backgroundColor: "#04d361",
+    backgroundColor: '#04d361',
     paddingHorizontal: 10,
     paddingVertical: 5,
-    color: "#fff",
+    color: '#fff'
   },
   likesContainer: {
     marginTop: 15,
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   },
   likeText: {
     fontSize: 14,
-    fontWeight: "bold",
-    marginRight: 10,
+    fontWeight: 'bold',
+    marginRight: 10
   },
   button: {
-    marginTop: 10,
+    marginTop: 10
   },
   buttonText: {
     fontSize: 14,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginRight: 10,
-    color: "#fff",
-    backgroundColor: "#7159c1",
-    padding: 15,
-  },
-});
+    color: '#fff',
+    backgroundColor: '#7159c1',
+    padding: 15
+  }
+})
